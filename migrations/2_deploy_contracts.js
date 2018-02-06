ed666129b9f6197129e870ce3a0d2a72ecda521a
originaltbs <4267343+originaltbs@users.noreply.github.com>
2018-02-06 01:05:39 -0500

    Fix migration constructor parameters.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,5 +2,5 @@ var Scythereum = artifacts.require("./Scythereum.sol");
 
 module.exports = function(deployer) {
   //deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(Scythereum, 1000, "Scythes", "CYTH", 10, 2);
+  deployer.deploy(Scythereum, "Scythes", "CYTH", 10, 4);
 };
